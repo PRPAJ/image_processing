455a1f3f1cc2a99e5e0019a2bb383b2bb9b9d571
PAULO JR <paulo.pj.alves@gmail.com>
2022-10-17 13:24:52 -0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup,find_packages
 
 with open("README.md", "r") as f:
     page_description = f.read()
@@ -7,15 +7,16 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
-    version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
-    long_description=page_description,
-    long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
-    packages=find_packages(),
-    install_requires=requirements,
-    python_requires='>=3.8',
+name="image_processing",
+version="0.0.1",author="Paulo Alves",
+author_email="paulo.pj.alves@gmail.com",
+description="Estudo sobre como subir um pacote. Projeto copiado de Karina Kato",
+long_description = page_description,
+long_description_content_type = "text/markdown",
+url="https://github.com/PRPAJ/image_processing",
+packages=find_packages(),
+install_requires=requirements,
+python_requires='>=3.5',
 )
+
+    
