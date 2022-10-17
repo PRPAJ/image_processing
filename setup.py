d5ed48c6c7b1b0527f7e66628dc99914446531b3
PAULO JR <paulo.pj.alves@gmail.com>
2022-10-17 13:28:54 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup,find_packages
 with open("README.md", "r") as f:
     page_description = f.read()
 
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
+'''with open("requirements.txt") as f:
+    requirements = f.read().splitlines()'''
 
 setup(
 name="image_processing",
@@ -15,7 +15,7 @@ long_description = page_description,
 long_description_content_type = "text/markdown",
 url="https://github.com/PRPAJ/image_processing",
 packages=find_packages(),
-install_requires=requirements,
+#install_requires=requirements,
 python_requires='>=3.5',
 )
 
